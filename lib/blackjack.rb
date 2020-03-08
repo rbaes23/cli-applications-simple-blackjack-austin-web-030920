02980bc7c97a5ef720f9da99abf3cd92d8f2a7e5
Rochelle Bondoc <rochelleannebaes@gmail.com>
2020-03-08 19:36:16 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -30,26 +30,45 @@ def end_game(card_total)
   puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
-# def initial_round
-#   # code #initial_round here
-#   card_total = deal_card
-#   card_total += deal_card
-#   display_card_total(card_total)
-# end
+def initial_round
+  # code #initial_round here
+  card_total = deal_card + deal_card
+  display_card_total(card_total)
+  card_total
+end
 
-# def hit?
-#   # code hit? here
-# end
+def hit?(current_total)
+  # code hit? here
+  prompt_user
+  input = get_user_input
+  if input == 's'
+    current_total
+  elsif input == 'h'
+    current_total += deal_card
+  else
+    invalid_command
+    prompt_user
+    get_user_input
+  end
+end
 
-# def invalid_command
-#   # code invalid_command here
-# end
+def invalid_command
+  # code invalid_command here
+  puts "Please enter a valid command"
+end
 
-# #####################################################
-# # get every test to pass before coding runner below #
-# #####################################################
+#####################################################
+# get every test to pass before coding runner below #
+#####################################################
 
-# def runner
-#   # code runner here
-# end
+def runner
+  # code runner here
+  welcome
+  current_total = initial_round
+  until current_total > 21
+    current_total = hit?(current_total)
+    display_card_total(current_total)
+  end
+  end_game(current_total)
+end
     
--- spec/blackjack_spec.rb
+++ spec/blackjack_spec.rb
@@ -55,84 +55,84 @@ describe "#end_game" do
   end
 end
 
-# describe "#initial_round" do
+describe "#initial_round" do
 
-#   it "calls on #deal_card twice and returns the sum" do
-#     expect(self).to receive(:deal_card).at_least(:twice).and_return(6)
-#     expect(initial_round).to eq(12)
-#   end
+  it "calls on #deal_card twice and returns the sum" do
+    expect(self).to receive(:deal_card).at_least(:twice).and_return(6)
+    expect(initial_round).to eq(12)
+  end
 
-#   it "calls on the '#display_card_total' to print sum of cards" do
-#     expect(self).to receive(:deal_card).at_least(:twice).and_return(6)
-#     expect($stdout).to receive(:puts).with(/Your cards add up to /)
-#     initial_round
-#   end
+  it "calls on the '#display_card_total' to print sum of cards" do
+    expect(self).to receive(:deal_card).at_least(:twice).and_return(6)
+    expect($stdout).to receive(:puts).with(/Your cards add up to /)
+    initial_round
+  end
 
-# end
+end
 
-# describe "invalid_command" do
-#   it "prints 'Please enter a valid command' when called" do
-#     expect($stdout).to receive(:puts).with("Please enter a valid command")
-#     invalid_command
-#   end
-# end
+describe "invalid_command" do
+  it "prints 'Please enter a valid command' when called" do
+    expect($stdout).to receive(:puts).with("Please enter a valid command")
+    invalid_command
+  end
+end
 
-# describe "#hit?" do
-#   it "calls on #prompt_user then #get_user_input" do
-#     expect($stdout).to receive(:puts).with("Type 'h' to hit or 's' to stay")
-#     expect(self).to receive(:get_user_input).and_return("s")
-#     hit?(7)
-#   end
+describe "#hit?" do
+  it "calls on #prompt_user then #get_user_input" do
+    expect($stdout).to receive(:puts).with("Type 'h' to hit or 's' to stay")
+    expect(self).to receive(:get_user_input).and_return("s")
+    hit?(7)
+  end
 
-#   it "returns an integer which is the card total" do
-#     expect(self).to receive(:get_user_input).and_return("s")
-#     expect(hit?(20)).to eq(20)
-#   end
+  it "returns an integer which is the card total" do
+    expect(self).to receive(:get_user_input).and_return("s")
+    expect(hit?(20)).to eq(20)
+  end
 
-#   it "does not deal another card if user types 's'" do
-#     expect(self).to receive(:get_user_input).and_return("s")
-#     expect(hit?(7)).to eq(7)
-#   end
+  it "does not deal another card if user types 's'" do
+    expect(self).to receive(:get_user_input).and_return("s")
+    expect(hit?(7)).to eq(7)
+  end
 
-#   it "deals another card when user types 'h'" do
-#     expect(self).to receive(:get_user_input).and_return("h")
-#     expect(self).to receive(:deal_card).and_return(7)
-#     expect(hit?(7)).to eq(14)
-#   end
+  it "deals another card when user types 'h'" do
+    expect(self).to receive(:get_user_input).and_return("h")
+    expect(self).to receive(:deal_card).and_return(7)
+    expect(hit?(7)).to eq(14)
+  end
 
-#   it "displays the invalid command message and prompts the user again if a user inputs something other than 'h' or 's'" do
-#     expect($stdout).to receive(:puts).with("Type 'h' to hit or 's' to stay")
-#     expect($stdout).to receive(:puts).with("Please enter a valid command")
-#     expect($stdout).to receive(:puts).with("Type 'h' to hit or 's' to stay")
-#     expect(self).to receive(:get_user_input).and_return("k")
-#     expect(self).to receive(:get_user_input).and_return("s")
+  it "displays the invalid command message and prompts the user again if a user inputs something other than 'h' or 's'" do
+    expect($stdout).to receive(:puts).with("Type 'h' to hit or 's' to stay")
+    expect($stdout).to receive(:puts).with("Please enter a valid command")
+    expect($stdout).to receive(:puts).with("Type 'h' to hit or 's' to stay")
+    expect(self).to receive(:get_user_input).and_return("k")
+    expect(self).to receive(:get_user_input).and_return("s")
     
-#     hit?(7)
-#   end
+    hit?(7)
+  end
 
-# end
+end
 
-# describe "#runner" do
-#   before(:each) do
-#     def get_user_input
-#       "h"
-#     end 
-#   end
+describe "#runner" do
+  before(:each) do
+    def get_user_input
+      "h"
+    end 
+  end
 
-#   it "calls on the #welcome method, 
-#   then on the #initial_round method, 
-#   then calls #hit? and #display_card_total methods
-#   -until- the card sum is greater than 21,
-#   then calls on the #end_game method" do
+  it "calls on the #welcome method, 
+  then on the #initial_round method, 
+  then calls #hit? and #display_card_total methods
+  -until- the card sum is greater than 21,
+  then calls on the #end_game method" do
 
-#     expect(self).to receive(:deal_card).at_least(3).times.and_return(10)
-#     expect(self).to receive(:get_user_input).and_return("h")
+    expect(self).to receive(:deal_card).at_least(3).times.and_return(10)
+    expect(self).to receive(:get_user_input).and_return("h")
 
-#     expect($stdout).to receive(:puts).with("Welcome to the Blackjack Table")
-#     expect($stdout).to receive(:puts).with("Your cards add up to 20")
-#     expect($stdout).to receive(:puts).with("Type 'h' to hit or 's' to stay")
-#     expect($stdout).to receive(:puts).with("Your cards add up to 30")
-#     expect($stdout).to receive(:puts).with("Sorry, you hit 30. Thanks for playing!")
-#     runner
-#   end
-# end
+    expect($stdout).to receive(:puts).with("Welcome to the Blackjack Table")
+    expect($stdout).to receive(:puts).with("Your cards add up to 20")
+    expect($stdout).to receive(:puts).with("Type 'h' to hit or 's' to stay")
+    expect($stdout).to receive(:puts).with("Your cards add up to 30")
+    expect($stdout).to receive(:puts).with("Sorry, you hit 30. Thanks for playing!")
+    runner
+  end
+end
