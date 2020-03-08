9c98c33709cd36798c7a3981fadc27ce829b6235
Rochelle Bondoc <rochelleannebaes@gmail.com>
2020-03-08 19:22:33 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -53,7 +53,7 @@ end
 
 def invalid_command
   # code invalid_command here
-  print "Please enter a valid command"
+  puts "Please enter a valid command"
 end
 
 #####################################################
